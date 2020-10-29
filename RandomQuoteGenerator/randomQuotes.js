6dda21e7f1d85899639f01bbce4374dfeea82cbf
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-10-28 22:12:25 -0700

    1

--- RandomQuoteGenerator/randomQuotes.js
+++ RandomQuoteGenerator/randomQuotes.js
@@ -1,14 +1,25 @@
 var quotes = [
-    "That might be poop",
-    "Oh that's poop",
+    "That might be poop.",
+    "Oh that's poop.",
     "Wait, maybe that's just dirt?",
-    "Nevermind it's just a coffee stain."
+    "Nevermind it's just a coffee stain.",
+    "Actually, I'm just dreaming."
 ];
-
-var random = Math.floor(Math.random()* quotes.length);
-
-function printQuotes(q){
-    console.log(q);
+function generate(arr){
+    var random = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quoteDisplay").innerHTML = quotes[random];        
 }
 
-printQuotes(quotes[random]);
+
+function addQuote() {
+    let addInput = quotes;
+    let add = document.getElementById("userinput").value;
+    if(add == ""){
+        alert("Please add a quote!");
+    }
+    else{
+      addInput.push(add);
+      document.getElementById("userinput").value = "";
+    };
+  
+  };
